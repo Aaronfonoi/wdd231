01b802912df5da53fb00f587790491baa8b68126
Aaronfonoi <aaronfonoi@gmail.com>
2026-01-19 19:24:36 -0800

    week 2 work

---
+++ scripts/ward.js
@@ -0,0 +1,28 @@
+{
+    "family_name": "Fonoimoana",
+    "move-in": "2026-01-13",
+    "number_of_people": "4",
+    "visited": true,
+    "family_members": [
+        {
+            "name": "Aaron Fonoimoana",
+            "gender": "Male",
+            "birthdate": "1998-04-16"
+        },
+        {
+             "name": "Audrey Fonoimoana",
+            "gender": "Female",
+            "birthdate": "1996-08-03"
+        },
+         {
+             "name": "Alio Fonoimoana",
+            "gender": "Male",
+            "birthdate": "2022-09-11"
+        },
+         {
+             "name": "Josephine Fonoimoana",
+            "gender": "Female",
+            "birthdate": "2024-09-16"
+        }
+    ]
+}
